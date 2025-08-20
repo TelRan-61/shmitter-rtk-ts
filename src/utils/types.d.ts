70ebb0fee93b7c454a7e9dcb98cdae9e30fdc8ae
edward <edwardlfish@gmail.com>
2025-08-20 21:05:59 +0300

    Update Homework tasks

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -2,7 +2,7 @@ export interface Stats {
     followers: number,
     following: number,
 }
-// Homework 2 instead of constants folllowers and following, use enum
+// TODO Homework 2 instead of constants followers and following, use enum
 export enum StatsType {
     FOLLOWERS = 'followers',
     FOLLOWING = 'following',
